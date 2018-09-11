d559bb8f4b9ca9b44b2cb1c053a1ebc941e4d446
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-12 00:08:01 +0200

    Added CategoryController and CategoryService
    Added CategoryController and CategoryService + tests

--- src/main/java/netherwulf/springfamework/services/CategoryService.java
+++ src/main/java/netherwulf/springfamework/services/CategoryService.java
@@ -1,4 +1,12 @@
 package netherwulf.springfamework.services;
 
+import netherwulf.springfamework.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
 }
